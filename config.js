ed0bddb38d6f8305ae35290ecc17a819ffe14ec7
ssafytree-spec <ssafytree@gmail.com>
2026-07-20 15:14:06 +0900

    Hash 값

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://fnkmthbpksmtfvcrkwyr.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZua210aGJwa3NtdGZ2Y3Jrd3lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxNDUzMDksImV4cCI6MjA5OTcyMTMwOX0.QEyItLqgyGAjg9BezZx4rLsWzTvtBnVTuFpA-0WhrBI",
-  ACCESS_CODE: "ssafy",
+
+  // 접속코드 원문 대신 해시값을 저장합니다 (지금 값은 "ssafy"의 해시).
+  // 접속코드를 바꾸려면 같이 드린 hash-generator.html을 열어서 새 해시를 만들어 여기 붙여넣으세요.
+  ACCESS_CODE_HASH: "daa565e94a765520362474fe25806000f502042e1cfdcd08107d9467a6e5edc3",
 };
